663b60c2bcbc7d807fe283ca500fb364c2129ac3
noby <n.fujioka@abacusmaths.info>
2016-08-03 07:14:49 +0100

    airport does not allow plen to land when it is full at capacity.

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -4,14 +4,23 @@ require 'plane'
 
 describe Airport do
   it 'so planes land at airports, instruct a plane to land' do
-    airport = Airport.new
+    airport = Airport.new(20)
     plane = Plane.new
     expect { airport.land(plane)}.not_to raise_error
   end
   # want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
   it 'so planes take off from airports, instruct a plane to take off' do
-    airport = Airport.new
+    airport = Airport.new(20)
     plane = Plane.new
     expect { airport.take_off(plane) }.not_to raise_error
+  end
+  # I want to prevent landing when the airport is full
+  it 'does not allow planes to land when airport is full' do
+    airport = Airport.new(20)
+    plane = Plane.new
+    20.times do
+      airport.land(plane)
+    end
+      expect { airport.land(plane) }.to raise_error 'Cannot land plane: airport full'
   end
 end
